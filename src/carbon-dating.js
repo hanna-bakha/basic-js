5491ec0e6ce537d9e4ef53d20f4d7eb99995e92c
Hanna Bakhanovich <ann.bakhanovich@mail.ru>
2021-09-12 20:03:56 +0300

    feat: add code

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,7 +17,12 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-export default function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
+ export default function dateSample(sampleActivity) {
   // remove line with error and write your code here
-}
+  if (typeof(sampleActivity) !== 'string' || +sampleActivity > 15 || +sampleActivity <= 0 || isNaN(+sampleActivity)) {
+  return false;
+  } else {
+  let t = Math.ceil(Math.log(MODERN_ACTIVITY / +sampleActivity) / (Math.log(2) / HALF_LIFE_PERIOD));
+  return +t;
+  }
+  }
